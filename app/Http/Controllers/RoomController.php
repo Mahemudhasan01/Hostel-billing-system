7e487d3c5939a95582e4c292bcf86c08d7722d4e
Mahemudhasan01 <m.m.vadhaniya1@gmail.com>
2024-03-17 18:34:13 +0530

    Changes

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -20,23 +20,51 @@ class RoomController extends Controller
         return view('admin_views.room-list', ['rooms' => $rooms, 'i' => $i]);
     }
 
-    function addRoom(Request $request){
+    function addRoom($editId, Request $request){
+        
         $msg = Validator::make($request->all(), [
             'room_name' => 'required',
             'room_fees' => 'required'
         ]);
 
         if($msg->fails()){
-            return back()->with('errors', $msg->errors());
+            return back()->with('error', 'Please fill all required field!.');
         }else{
-            DB::table('rooms')->insert([
-                'room' => $request->room_name,
-                'description' => $request->room_desc,
-                'fees' => $request->room_fees,
-                'created_at' => date('Y-m-d')
-            ]);
+            if($editId != 0){
+                DB::table("rooms")
+                    ->where("id", "=", $editId)
+                    ->update([
+                    'room' => $request->room_name,
+                    'description' => $request->room_desc,
+                    'fees' => $request->room_fees,
+                    'created_at' => date('Y-m-d')
+                ]);
 
-            return back()->with('success', "Student Add Successfuly");
+                return redirect()->route('show.manage.room')->with('success', 'Room updated successfully');
+            }else{
+                DB::table('rooms')->insert([
+                    'room' => $request->room_name,
+                    'description' => $request->room_desc,
+                    'fees' => $request->room_fees,
+                    'created_at' => date('Y-m-d')
+                ]);
+                return redirect()->route('show.manage.room')->with('success', 'Room create successfully');
+            }
+        }
+    }
+
+    function editRoom($id){
+        $room = DB::table("rooms")->find("$id");
+        
+        return view('admin_views.add-rooms', ['roomDtls' => $room]);
+    }
+
+    function deleteRoom($id){
+        // dd($id);
+        if($id){
+            DB::table("rooms")->delete($id);
+
+            return redirect()->route('show.manage.room')->with('success', 'Room delete successfully');
         }
     }
 }
